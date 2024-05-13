03baa74a9244dd60fb84f8b94b17cec200fdebd2
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-05-13 20:57:20 +0530

    okokokoko

--- section5/ok.py
+++ section5/ok.py
@@ -7,7 +7,7 @@ c=int(input('enter c : '))
 root1=(-b+ math.sqrt(b**2-4*a*c))/(2*a)
 root2=(-b- math.sqrt(b**2-4*a*c))/(2*a)
 print('root are:',root1,root2)
-ok i done it
+# ok i done it
 # with this snippet:        
 # a=int(input('enter a number: '))
 # if a>0:

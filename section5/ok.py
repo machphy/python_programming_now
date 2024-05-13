bd536392c726cedd74048af552986d5adf8f30c2
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-05-13 20:59:13 +0530

    111jjjjj1j1

--- section5/ok.py
+++ section5/ok.py
@@ -9,11 +9,11 @@ root2=(-b- math.sqrt(b**2-4*a*c))/(2*a)
 print('root are:',root1,root2)
 # ok i done it
 # with this snippet:        
-# a=int(input('enter a number: '))
-# if a>0:
-#     print('positive')
-# else:
-#     print('negative')
+a=int(input('enter a number: '))
+if a>0:
+    print('positive')
+else:
+    print('negative')
 # What is the difference between the two snippets?
 
 

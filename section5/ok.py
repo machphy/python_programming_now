a4ad7c8caba7fe0fdb6fec216b1f8bdf4a0d32d6
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-05-13 20:55:02 +0530

    ok now ofdate

--- section5/ok.py
+++ section5/ok.py
@@ -7,3 +7,14 @@ c=int(input('enter c : '))
 root1=(-b+ math.sqrt(b**2-4*a*c))/(2*a)
 root2=(-b- math.sqrt(b**2-4*a*c))/(2*a)
 print('root are:',root1,root2)
+ok i done it
+# with this snippet:        
+# a=int(input('enter a number: '))
+# if a>0:
+#     print('positive')
+# else:
+#     print('negative')
+# What is the difference between the two snippets?
+
+
+# A. The first snippet calculates the roots of a quadratic equation, while the second snippet checks if a number is positive or negative.git
